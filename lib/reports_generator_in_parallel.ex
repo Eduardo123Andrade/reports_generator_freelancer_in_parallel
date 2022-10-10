63714178425b921e483b42260c350d82bd462a10
Eduardo <eduardo.andrade15@hotmail.com>
2022-10-10 13:34:24 -0300

    first commit

---
+++ lib/reports_generator_in_parallel.ex
@@ -0,0 +1,18 @@
+defmodule ReportsGeneratorInParallel do
+  @moduledoc """
+  Documentation for `ReportsGeneratorInParallel`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReportsGeneratorInParallel.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/reports_generator_in_parallel_test.exs
@@ -0,0 +1,8 @@
+defmodule ReportsGeneratorInParallelTest do
+  use ExUnit.Case
+  doctest ReportsGeneratorInParallel
+
+  test "greets the world" do
+    assert ReportsGeneratorInParallel.hello() == :world
+  end
+end
